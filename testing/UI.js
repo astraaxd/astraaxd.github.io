e9e660e5ce5e7bb289c29be1936c90bc26cc8909
Astraa <166195056+astraaxd@users.noreply.github.com>
2025-02-06 19:34:24 -0600

    Update UI.js

--- testing/UI.js
+++ testing/UI.js
@@ -1,24 +1,13 @@
 /**
- * @license AGPL-3.0
- * Blooket Cheats
- * Copyright (C) 2023-present 05Konz
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * Source: https://github.com/Blooket-Council/Blooket-Cheats 05konz994@gmail.com
+ * AstraaXD's Blooket GUI
+ * Copyright (C) 2023-present 05Konz (and i guess astraa?)
+ *
+ *
+ * idk man, this is just an edited version of 05Konz blooket GUI
+ *
 */
 
-/* THE UPDATE CHECKER IS ADDED DURING COMMIT PREP, THERE MAY BE REDUNDANT CODE, DO NOT TOUCH */
+// P.S: The update checker isn't in this GUI, since the update checker broke it before!!!
 
 (() => {
     let iframe = document.querySelector("iframe");
